338b829d693500fd317648417fef245498e257f3
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2025-01-08 16:52:53 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -59,11 +59,11 @@
         if ($(this).scrollTop() > 50) {
             // $('.back-to-top').fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            // $('.back-to-top').fadeOut('slow');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        // $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
 
