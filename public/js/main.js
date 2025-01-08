86f6a2707a0bdcff1a8026f056f1e7373ebf43e5
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2025-01-08 16:51:56 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -57,7 +57,7 @@
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
-            $('.back-to-top').fadeIn('slow');
+            // $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
