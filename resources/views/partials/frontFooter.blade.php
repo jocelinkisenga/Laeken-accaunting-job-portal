a899f79f9d827165ee5a67d844a257157061fbc9
jocelin kisenga <kisengajocelin7@gmail.com>
2025-09-21 22:10:14 +0200

    udated navbar

--- resources/views/partials/frontFooter.blade.php
+++ resources/views/partials/frontFooter.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer py-3 mt-5">
+<footer class="footer py-3 fixed-bottom mt-5">
     <div class="container container-max text-center">
         <p class="mb-1"><strong>Laeken Consulting</strong> — Connecting finance talent with opportunities.</p>
         <p class="small-muted mb-0">© 2025 Laeken consulting. Tous droits réservés.</p>
